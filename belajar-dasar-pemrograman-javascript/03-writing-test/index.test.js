16d26d44cbc82a21a84b3cf2f82f74726404d35f
dianerdiana <dianerdiana.de@gmail.com>
2025-08-21 10:57:11 +0700

    Complete task 03 write test

--- belajar-dasar-pemrograman-javascript/03-writing-test/index.test.js
+++ belajar-dasar-pemrograman-javascript/03-writing-test/index.test.js
@@ -2,47 +2,90 @@ import { describe, it } from "node:test";
 import assert from "node:assert";
 import { sum } from "./index.js";
 
-// Arrange
-// Action
-// Assert
-
-describe("Calculator", () => {
-  it("Should get correct result", () => {
+describe("Positive Scenario", () => {
+  it("Should return 12 when adding 5 and 7", () => {
     // Arrange
-    const operandA = 5;
-    const operandB = 7;
+    const a = 5;
+    const b = 7;
 
     // Action
-    const actualValue = sum(operandA, operandB);
+    const result = sum(a, b);
 
     // Assert
     const expectedValue = 12;
-    assert.equal(actualValue, expectedValue);
+    assert.equal(result, expectedValue);
   });
 
-  it("Should return a string", () => {
+  it("Should handle negative numbers", () => {
     // Arrange
-    const operandA = "4";
-    const operandB = 7;
+    const a = 5;
+    const b = -7;
 
     // Action
-    const actualValue = sum(operandA, operandB);
+    const result = sum(a, b);
 
     // Assert
-    const expectedValue = 11;
-    assert.notStrictEqual(actualValue, expectedValue);
+    const expectedValue = -2;
+    assert.equal(result, expectedValue);
   });
 
-  it("Should return correct result", () => {
+  it("Should handle decimals", () => {
     // Arrange
-    const operandA = -5;
-    const operandB = 7;
+    const a = 2.34;
+    const b = 7.21;
 
     // Action
-    const actualValue = sum(operandA, operandB);
+    const result = sum(a, b);
 
     // Assert
-    const expectedValue = 2;
-    assert.strictEqual(actualValue, expectedValue);
+    const expectedValue = 9.55;
+    assert.equal(result, expectedValue);
+  });
+});
+
+describe("Neegative Scenario Cases", () => {
+  it("Should concatenate when one or both of input is not a number", () => {
+    // Arrange
+    const a = 4;
+    const b = "Budi";
+
+    // Action
+    const result = sum(a, b);
+
+    // Assert
+    const expectedValue = "4Budi";
+    assert.strictEqual(result, expectedValue);
+  });
+
+  it("Should return NaN when one or both of input is undefined", () => {
+    // Arrange
+    const a = undefined;
+    const b = 2;
+
+    // Action
+    const result = sum(a, b);
+
+    // Assert
+    assert.ok(Number.isNaN(result));
+  });
+
+  it("Should return 0 when input is null", () => {
+    // Action
+    const result = sum(null, null);
+
+    // Assert
+    assert.strictEqual(result, 0);
+  });
+
+  it("Should return 2 when one of input is null", () => {
+    // Arrange
+    const a = null;
+    const b = 2;
+
+    // Action
+    const result = sum(a, b);
+
+    // Assert
+    assert.strictEqual(result, 2);
   });
 });
