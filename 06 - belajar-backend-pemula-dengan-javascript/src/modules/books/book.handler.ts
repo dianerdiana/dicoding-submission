ba7f505099ff3d58b6afc3e9f9838b7a190c8929
dianerdiana <dianerdiana.de@gmail.com>
2025-10-18 22:00:16 +0700

    Update (module): implement global error handler to book handler

--- 06 - belajar-backend-pemula-dengan-javascript/src/modules/books/book.handler.ts
+++ 06 - belajar-backend-pemula-dengan-javascript/src/modules/books/book.handler.ts
@@ -1,22 +1,22 @@
 import BookService from './book.service';
 import type { HapiHandler } from '../../types/hapi';
 import { ResponseObject } from '@hapi/hapi';
-import { errorResponse, successResponse } from '../../utils/response';
+import { successResponse } from '../../utils/response';
 import {
   CreateBookPayload,
   createBookSchema,
   UpdateBookPayload,
   updateBookSchema,
 } from './book.schema';
-import z from 'zod';
+import { handleError } from '../../utils/handleError';
+import { NotFoundError } from '../../common/AppError';
 
 export default class BookHandler {
   private bookService = new BookService();
 
   createBook: HapiHandler = async (req, res): Promise<ResponseObject> => {
-    const payload = req.payload as CreateBookPayload;
-
     try {
+      const payload = req.payload as CreateBookPayload;
       await createBookSchema.parseAsync(payload);
       const bookId = await this.bookService.createBook(payload);
 
@@ -27,15 +27,7 @@ export default class BookHandler {
         code: 201,
       });
     } catch (error) {
-      if (error instanceof z.ZodError) {
-        return errorResponse({ res, message: error.issues[0].message, code: 400 });
-      }
-
-      if (error instanceof Error) {
-        return errorResponse({ res, message: error.message, code: 400 });
-      }
-
-      return errorResponse({ res, message: 'Server Error', code: 500 });
+      return handleError({ res, error });
     }
   };
 
@@ -44,61 +36,34 @@ export default class BookHandler {
       const books = await this.bookService.getAllBooks();
       return successResponse({ res, data: { books } });
     } catch (error) {
-      if (error instanceof Error) {
-        return errorResponse({ res, message: error.message, code: 404 });
-      }
-
-      return errorResponse({ res, message: 'Server Error', code: 500 });
+      return handleError({ res, error });
     }
   };
 
   getBookById: HapiHandler = async (req, res): Promise<ResponseObject> => {
-    const { bookId } = req.params;
-
     try {
-      if (!bookId) {
-        return errorResponse({ res, message: 'Buku tidak ditemukan', code: 404 });
-      }
+      const { bookId } = req.params;
+      if (!bookId) throw new NotFoundError('Buku tidak ditemukan');
 
       const book = await this.bookService.getBookById(bookId);
-
-      if (!book) {
-        return errorResponse({ res, message: 'Buku tidak ditemukan', code: 404 });
-      }
+      if (!book) throw new NotFoundError('Buku tidak ditemukan');
 
       return successResponse({ res, data: { book } });
     } catch (error) {
-      if (error instanceof Error) {
-        return errorResponse({ res, message: error.message, code: 404 });
-      }
-
-      return errorResponse({ res, message: 'Server Error', code: 500 });
+      return handleError({ res, error });
     }
   };
 
   updateBook: HapiHandler = async (req, res): Promise<ResponseObject> => {
-    const { bookId } = req.params;
-    const payload = req.payload as UpdateBookPayload;
-
     try {
-      if (!bookId) {
-        return errorResponse({
-          res,
-          message: 'Gagal memperbarui buku. Id tidak ditemukan',
-          code: 404,
-        });
-      }
+      const { bookId } = req.params;
+      const payload = req.payload as UpdateBookPayload;
+      if (!bookId) throw new NotFoundError('Gagal memperbarui buku. Id tidak ditemukan');
 
       await updateBookSchema.parseAsync(payload);
-      const book = await this.bookService.getBookById(bookId);
 
-      if (!book) {
-        return errorResponse({
-          res,
-          message: 'Gagal memperbarui buku. Id tidak ditemukan',
-          code: 404,
-        });
-      }
+      const book = await this.bookService.getBookById(bookId);
+      if (!book) throw new NotFoundError('Gagal memperbarui buku. Id tidak ditemukan');
 
       const updatedBook = await this.bookService.updateBook(book, payload);
 
@@ -108,49 +73,23 @@ export default class BookHandler {
         data: { book: updatedBook },
       });
     } catch (error) {
-      if (error instanceof z.ZodError) {
-        return errorResponse({ res, message: error.issues[0].message, code: 400 });
-      }
-
-      if (error instanceof Error) {
-        return errorResponse({ res, message: error.message, code: 400 });
-      }
-
-      return errorResponse({ res, message: 'Server Error', code: 500 });
+      return handleError({ res, error });
     }
   };
 
   deleteBook: HapiHandler = async (req, res): Promise<ResponseObject> => {
     try {
-      const bookId = req.params.bookId;
-
-      if (!bookId) {
-        return errorResponse({
-          res,
-          message: 'Buku gagal dihapus. Id tidak ditemukan',
-          code: 404,
-        });
-      }
+      const { bookId } = req.params;
+      if (!bookId) throw new NotFoundError('Buku gagal dihapus. Id tidak ditemukan');
 
       const book = await this.bookService.getBookById(bookId);
-
-      if (!book) {
-        return errorResponse({
-          res,
-          message: 'Buku gagal dihapus. Id tidak ditemukan',
-          code: 404,
-        });
-      }
+      if (!book) throw new NotFoundError('Buku gagal dihapus. Id tidak ditemukan');
 
       await this.bookService.deleteBook(bookId);
 
       return successResponse({ res, message: 'Buku berhasil dihapus' });
     } catch (error) {
-      if (error instanceof Error) {
-        return errorResponse({ res, message: error.message, code: 400 });
-      }
-
-      return errorResponse({ res, message: 'Server Error', code: 500 });
+      return handleError({ res, error });
     }
   };
 }
