e62b04750b154bc97f964a7c590a3d7d21513d3e
dianerdiana <dianerdiana.de@gmail.com>
2025-10-23 18:44:56 +0700

    Update (module): delete transaction because doesnt need for single query

--- 07 - belajar-fundamental-backend-dengan-javascript/open-music-01/src/modules/albums/album.repository.ts
+++ 07 - belajar-fundamental-backend-dengan-javascript/open-music-01/src/modules/albums/album.repository.ts
@@ -1,45 +1,41 @@
 import { db } from '../../database';
 import { AlbumEntity } from './album.entity';
-import { mapAlbumRowToEntity } from './album.mapper';
+import { AlbumRow, mapAlbumRowToEntity } from './album.mapper';
 
 export class AlbumRepository {
   private tableName = 'albums';
 
   async create(album: AlbumEntity): Promise<AlbumEntity | null> {
-    let newAlbum = null;
-    await db.transaction(async (client) => {
-      const res = await client.query(
-        `INSERT INTO ${this.tableName}(name,year) VALUES ($1,$2) RETURNING *`,
-        [album.name, album.year],
-      );
+    const result = await db.query<AlbumRow>(
+      `INSERT INTO ${this.tableName}(name, year) VALUES ($1, $2) RETURNING *`,
+      [album.name, album.year],
+    );
 
-      newAlbum = res ? res.rows[0] : null;
-    });
-    return newAlbum ? mapAlbumRowToEntity(newAlbum) : null;
+    const newAlbumRow = result.rows[0];
+    if (!newAlbumRow) return null;
+
+    return mapAlbumRowToEntity(newAlbumRow);
   }
 
   async findById(id: string): Promise<AlbumEntity | null> {
-    let album = null;
+    const result = await db.query<AlbumRow>(`SELECT * FROM ${this.tableName} WHERE id=$1`, [id]);
 
-    await db.transaction(async (client) => {
-      const res = await client.query(`SELECT * FROM ${this.tableName} WHERE id=$1`, [id]);
-      album = res ? res.rows[0] : null;
-    });
-    return album ? mapAlbumRowToEntity(album) : null;
+    const existingAlbum = result.rows[0];
+    if (!existingAlbum) return null;
+
+    return mapAlbumRowToEntity(existingAlbum);
   }
 
   async update(id: string, album: AlbumEntity): Promise<AlbumEntity | null> {
-    let updatedAlbum = null;
+    const result = await db.query<AlbumRow>(
+      `UPDATE ${this.tableName} SET name=$1,year=$2 WHERE id=$3 RETURNING *`,
+      [album.name, album.year, id],
+    );
 
-    await db.transaction(async (client) => {
-      const res = await client.query(
-        `UPDATE ${this.tableName} SET name=$1,year=$2 WHERE id=$3 RETURNING *`,
-        [album.name, album.year, id],
-      );
-      updatedAlbum = res ? res.rows[0] : null;
-    });
+    const existingAlbum = result.rows[0];
+    if (!existingAlbum) return null;
 
-    return updatedAlbum ? mapAlbumRowToEntity(updatedAlbum) : null;
+    return mapAlbumRowToEntity(existingAlbum);
   }
 
   async delete(id: string): Promise<void> {
