91ad04b92417ad40c9a643d9afb8ff5673340af4
dianerdiana <dianerdiana.de@gmail.com>
2025-10-24 17:46:50 +0700

    Update (common): create new class error handling BadRequestError

--- 07 - belajar-fundamental-backend-dengan-javascript/open-music-02/src/common/AppError.ts
+++ 07 - belajar-fundamental-backend-dengan-javascript/open-music-02/src/common/AppError.ts
@@ -16,6 +16,11 @@ export class ValidationError extends AppError {
   }
 }
 
+export class BadRequestError extends AppError {
+  constructor(message: string) {
+    super(message, 400);
+  }
+}
 export class UnauthorizedError extends AppError {
   constructor(message: string) {
     super(message, 401);
