e16f440d7928f587ba1e39adbb1ef4e5b8bf3eb7
dianerdiana <dianerdiana.de@gmail.com>
2025-11-07 10:05:20 +0700

    Update (album): mapped album entity before saving it

--- 07 - belajar-fundamental-backend-dengan-javascript/open-music-03/src/modules/album/infrastructure/album.repository.ts
+++ 07 - belajar-fundamental-backend-dengan-javascript/open-music-03/src/modules/album/infrastructure/album.repository.ts
@@ -1,10 +1,12 @@
 import { db } from '../../../shared/libs/db';
 import { Album } from '../domain/entities/album.entity';
 import { AlbumId } from '../domain/value-objects/album-id.vo';
-import { AlbumRow, mapAlbumRowToEntity } from './album.mapper';
+import { AlbumRow, mapAlbumEntityToRow, mapAlbumRowToEntity } from './album.mapper';
 
 export class AlbumRepository {
   async save(album: Album): Promise<void> {
+    const mappedAlbum = mapAlbumEntityToRow(album);
+
     await db.query<AlbumRow>(
       `INSERT INTO albums (id, name, year, created_at, updated_at)
        VALUES ($1, $2, $3, $4, $5)
@@ -13,11 +15,11 @@ export class AlbumRepository {
        RETURNING *
        `,
       [
-        album.getId().toString(),
-        album.getName(),
-        album.getYear(),
-        album.getCreatedAt(),
-        album.getUpdatedAt(),
+        mappedAlbum.id,
+        mappedAlbum.name,
+        mappedAlbum.year,
+        mappedAlbum.created_at,
+        mappedAlbum.updated_at,
       ],
     );
   }
