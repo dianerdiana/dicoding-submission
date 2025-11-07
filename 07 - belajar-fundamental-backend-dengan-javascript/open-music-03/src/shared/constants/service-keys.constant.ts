0b8815096bb512a27d4039bbab7ebcd2bfff4360
dianerdiana <dianerdiana.de@gmail.com>
2025-11-07 23:33:58 +0700

    Update (constants): add  sign in user use-case constants to service-keys

--- 07 - belajar-fundamental-backend-dengan-javascript/open-music-03/src/shared/constants/service-keys.constant.ts
+++ 07 - belajar-fundamental-backend-dengan-javascript/open-music-03/src/shared/constants/service-keys.constant.ts
@@ -2,6 +2,7 @@ export const SERVICE_KEYS = {
   GET_ALL_SONGS_USE_CASE: 'GetAllSongsUseCase',
   JWT_SERVICE: 'JwtService',
   PASSWORD_SERVICE: 'PasswordService',
+  SIGN_IN_USER_USE_CASE: 'SignInUserUseCase',
 } as const;
 
 export type ServiceKey = (typeof SERVICE_KEYS)[keyof typeof SERVICE_KEYS];
