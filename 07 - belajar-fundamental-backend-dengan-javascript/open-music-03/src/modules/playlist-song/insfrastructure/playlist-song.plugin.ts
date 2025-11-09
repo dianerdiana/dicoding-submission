c201eb13c37052da4386ba2588767f9113cb4faf
dianerdiana <dianerdiana.de@gmail.com>
2025-11-09 22:15:06 +0700

    Update (playlist-song): register new use-case to plugin

--- 07 - belajar-fundamental-backend-dengan-javascript/open-music-03/src/modules/playlist-song/insfrastructure/playlist-song.plugin.ts
+++ 07 - belajar-fundamental-backend-dengan-javascript/open-music-03/src/modules/playlist-song/insfrastructure/playlist-song.plugin.ts
@@ -3,6 +3,7 @@ import { PlaylistSongRoute } from '../interface/http/playlist-song.route';
 import { PlaylistSongHandler } from '../interface/http/playlist-song.handler';
 import { PlaylistSongRepository } from './playlist-song.repository';
 import { AddSongToPlaylistUseCase } from '../application/use-case/add-song-to-playlist.use-case';
+import { GetPlaylistSongsUseCase } from '../application/use-case/get-playlist-songs.use-case';
 
 export const playlistSongPlugin: Plugin<undefined> = {
   name: 'playlistSongs',
@@ -10,7 +11,11 @@ export const playlistSongPlugin: Plugin<undefined> = {
   register: async (server) => {
     const playlistSongRepository = new PlaylistSongRepository();
     const addSongToPlaylistUseCase = new AddSongToPlaylistUseCase(playlistSongRepository);
-    const playlistSongHandler = new PlaylistSongHandler(addSongToPlaylistUseCase);
+    const getPlaylistSongsUseCase = new GetPlaylistSongsUseCase(playlistSongRepository);
+    const playlistSongHandler = new PlaylistSongHandler(
+      addSongToPlaylistUseCase,
+      getPlaylistSongsUseCase,
+    );
 
     server.route(new PlaylistSongRoute(playlistSongHandler).routes());
   },
